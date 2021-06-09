5ae944a905f8dd0f96e97ccf03ae216009a90588
Kinjal Raykarmakar <kinjalrk2k@gmail.com>
2021-06-09 23:28:06 +0530

    modified navbar

--- src/components/MyNavbar.jsx
+++ src/components/MyNavbar.jsx
@@ -6,10 +6,11 @@ function MyNavbar() {
       <Container fluid>
         <Navbar.Brand href="#home">Live HTML to PDF</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
+        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+            {/* <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link> */}
+            <Navbar.Text>Made with ❤ by Kinjal Raykarmakar</Navbar.Text>
           </Nav>
         </Navbar.Collapse>
       </Container>
