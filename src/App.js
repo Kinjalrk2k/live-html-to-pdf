d968ae85463c42bac9657da7f0c4c4de7ee92994
Kinjal Raykarmakar <kinjalrk2k@gmail.com>
2021-06-10 13:00:15 +0530

    refractored application in a seperate component

--- src/App.js
+++ src/App.js
@@ -1,38 +1,10 @@
-import Template from "./components/Template";
-import Options from "./components/Options";
-import Data from "./components/Data";
-import PdfDisplay from "./components/PdfDisplay";
-
+import Application from "./components/Application";
 import { HtmlPdfProvider } from "./contexts/HtmlPdf.context";
-import { Container, Row, Tab, Tabs, Col } from "react-bootstrap";
-import MyNavbar from "./components/MyNavbar";
 
 function App() {
   return (
     <HtmlPdfProvider>
-      <div className="App">
-        <MyNavbar />
-        <Container fluid>
-          <Row>
-            <Col>
-              <Tabs defaultActiveKey="template" styles={{ height: "90vh" }}>
-                <Tab eventKey="template" title="Home">
-                  <Template />
-                </Tab>
-                <Tab eventKey="options" title="Options">
-                  <Options />
-                </Tab>
-                <Tab eventKey="data" title="Data">
-                  <Data />
-                </Tab>
-              </Tabs>
-            </Col>
-            <Col>
-              <PdfDisplay />
-            </Col>
-          </Row>
-        </Container>
-      </div>
+      <Application />
     </HtmlPdfProvider>
   );
 }
---
+++ src/components/Application.jsx
@@ -0,0 +1,37 @@
+import Template from "./Template";
+import Options from "./Options";
+import Data from "./Data";
+import PdfDisplay from "./PdfDisplay";
+
+import { Container, Row, Tab, Tabs, Col } from "react-bootstrap";
+import MyNavbar from "./MyNavbar";
+
+function Application() {
+  return (
+    <>
+      <MyNavbar />
+      <Container fluid>
+        <Row>
+          <Col>
+            <Tabs defaultActiveKey="template" styles={{ height: "90vh" }}>
+              <Tab eventKey="template" title="Home">
+                <Template />
+              </Tab>
+              <Tab eventKey="options" title="Options">
+                <Options />
+              </Tab>
+              <Tab eventKey="data" title="Data">
+                <Data />
+              </Tab>
+            </Tabs>
+          </Col>
+          <Col>
+            <PdfDisplay />
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+}
+
+export default Application;
