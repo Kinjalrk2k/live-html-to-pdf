e4848916034f07b2450b47b66e6eece85479cc91
Kinjal Raykarmakar <kinjalrk2k@gmail.com>
2021-07-11 14:38:03 +0530

    added firebase config and apikey in env

---
+++ src/config/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: "live-html-to-pdf.firebaseapp.com",
+  projectId: "live-html-to-pdf",
+  storageBucket: "live-html-to-pdf.appspot.com",
+};
+
+export default firebaseConfig;
