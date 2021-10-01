ccd13ccabcc5d90c3a3f15992b73c98acab771cc
souptik4572 <souptiksarkar4572@gmail.com>
2021-10-01 16:08:52 +0530

    segragated repeating routes in the form of routes and controllers

--- server/app.js
+++ server/app.js
@@ -1,10 +1,12 @@
-require("dotenv/config");
+// require("dotenv/config");
 
 const express = require("express");
 const Handlebars = require("handlebars");
 const mongoose = require("mongoose");
 const pdf = require("pdf-creator-node");
 const Project = require("./models/Project");
+
+const projectRoutes = require('./routes/project');
 
 const { decodeIDToken } = require("./middlewares/firebase");
 
@@ -111,57 +113,6 @@ app.post("/pull/:originalProjectId", decodeIDToken, async (req, res) => {
   return res.json({ template, data, options });
 });
 
-app.get("/project/:id", async (req, res) => {
-  const { id } = req.params;
-
-  console.log("Getting project ID", id);
-  const project = await Project.findById(id).populate("owner");
-  const forks = await Project.find({ forked: id }).populate("owner");
-
-  res.json({ ...project._doc, forks });
-});
-
-app.post("/project/:id", decodeIDToken, async (req, res) => {
-  const { id } = req.params;
-  const { template, data, options } = req.body;
-
-  const project = await Project.findById(id);
-  const owner = project.owner;
-
-  project.template = template;
-  project.data = data;
-  project.options = options;
-
-  if (owner === undefined) {
-    console.log("Can save 1");
-    project.owner = req.user._id;
-  } else if (owner.equals(req.user._id)) {
-    console.log("Can save 2");
-  } else {
-    console.log("Cannot save");
-    return res.status(403).json({ err: "You don't own this project" });
-  }
-
-  project.save();
-
-  // const updatedProject = await Project.findById(id).populate("owner");
-  const updatedProject = await Project.populate(project, { path: "owner" });
-  // console.log(updatedProject);
-
-  res.json(updatedProject);
-});
-
-app.delete("/project/:id", decodeIDToken, async (req, res) => {
-  const { id } = req.params;
-
-  await Project.findOneAndDelete({
-    _id: id,
-    owner: req.user._id,
-  });
-
-  res.json({ success: true });
-});
-
 app.get("/projects", decodeIDToken, async (req, res) => {
   let projects = [];
   if (req.user) {
@@ -172,10 +123,7 @@ app.get("/projects", decodeIDToken, async (req, res) => {
   return res.json(projects);
 });
 
-app.patch("/project/:id/title", decodeIDToken, async (req, res) => {
-  await Project.findByIdAndUpdate(req.params.id, { title: req.body.title });
-  res.json({ success: true });
-});
+app.use('/project', projectRoutes);
 
 const PORT = process.env.PORT || 5000;
 
