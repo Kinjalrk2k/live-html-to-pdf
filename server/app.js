ff5f8410f6ab66bb23e3e7644516e8cf19f82de2
souptik4572 <souptiksarkar4572@gmail.com>
2021-10-01 16:11:53 +0530

    added condig file initialisation

--- server/app.js
+++ server/app.js
@@ -1,126 +1,122 @@
-// require("dotenv/config");
+require('dotenv/config');
 
-const express = require("express");
-const Handlebars = require("handlebars");
-const mongoose = require("mongoose");
-const pdf = require("pdf-creator-node");
-const Project = require("./models/Project");
+const express = require('express');
+const Handlebars = require('handlebars');
+const mongoose = require('mongoose');
+const pdf = require('pdf-creator-node');
+const Project = require('./models/Project');
 
 const projectRoutes = require('./routes/project');
 
-const { decodeIDToken } = require("./middlewares/firebase");
+const { decodeIDToken } = require('./middlewares/firebase');
 
 const app = express();
 
-app.use(require("cors")());
+app.use(require('cors')());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const db = process.env.mongoURI;
 // Datebase connection
 mongoose
-  .connect(db, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  })
-  .then(() => console.log(`MongoDB connected successfully`))
-  .catch((err) => console.log(`Error connecting mongodb ` + err));
+	.connect(db, {
+		useUnifiedTopology: true,
+		useNewUrlParser: true,
+		useCreateIndex: true,
+		useFindAndModify: false,
+	})
+	.then(() => console.log(`MongoDB connected successfully`))
+	.catch((err) => console.log(`Error connecting mongodb ` + err));
 
-app.get("/", (req, res) => {
-  return res.json({ app: "Live HTML to PDF", developer: "Kinjal Raykarmakar" });
+app.get('/', (req, res) => {
+	return res.json({ app: 'Live HTML to PDF', developer: 'Kinjal Raykarmakar' });
 });
 
-app.post("/pdf", async (req, res) => {
-  let { template, options, data } = req.body;
+app.post('/pdf', async (req, res) => {
+	let { template, options, data } = req.body;
 
-  try {
-    options = JSON.parse(options);
-    data = JSON.parse(data);
+	try {
+		options = JSON.parse(options);
+		data = JSON.parse(data);
 
-    const document = {
-      html: template,
-      data,
-      path: "./output.pdf",
-      type: "buffer",
-    };
+		const document = {
+			html: template,
+			data,
+			path: './output.pdf',
+			type: 'buffer',
+		};
 
-    pdf.create(document, options).then((file) => {
-      // console.log(file);
-      return res.send(file);
-    });
-  } catch (e) {
-    console.log("ERROR: ", e.message);
-    return res.status(400).json({ err: e.message });
-  }
+		pdf.create(document, options).then((file) => {
+			// console.log(file);
+			return res.send(file);
+		});
+	} catch (e) {
+		console.log('ERROR: ', e.message);
+		return res.status(400).json({ err: e.message });
+	}
 });
 
-app.get("/new", decodeIDToken, async (req, res) => {
-  let query = {};
-  if (req.user) {
-    console.log("Owner:", req.user._id);
-    query["owner"] = req.user._id;
-  }
+app.get('/new', decodeIDToken, async (req, res) => {
+	let query = {};
+	if (req.user) {
+		console.log('Owner:', req.user._id);
+		query['owner'] = req.user._id;
+	}
 
-  const newProject = new Project(query);
-  const { _id } = await newProject.save();
-  console.log("Project created with ID:", _id);
+	const newProject = new Project(query);
+	const { _id } = await newProject.save();
+	console.log('Project created with ID:', _id);
 
-  return res.json({ projectId: _id, owner: req.user });
+	return res.json({ projectId: _id, owner: req.user });
 });
 
-app.get("/fork/:projectId", decodeIDToken, async (req, res) => {
-  if (!req.user) {
-    return res.status(401).json({ err: "You need to login to create a fork" });
-  }
+app.get('/fork/:projectId', decodeIDToken, async (req, res) => {
+	if (!req.user) {
+		return res.status(401).json({ err: 'You need to login to create a fork' });
+	}
 
-  const { template, data, options, title } = await Project.findById(
-    req.params.projectId
-  );
-  const newProject = new Project({
-    title: `Fork of ${title}`,
-    template,
-    data,
-    options,
-    owner: req.user._id,
-    forked: req.params.projectId,
-  });
-  const { _id } = await newProject.save();
+	const { template, data, options, title } = await Project.findById(req.params.projectId);
+	const newProject = new Project({
+		title: `Fork of ${title}`,
+		template,
+		data,
+		options,
+		owner: req.user._id,
+		forked: req.params.projectId,
+	});
+	const { _id } = await newProject.save();
 
-  console.log(
-    `Fork of ${req.body.projectId} was created. New Project ID: ${_id}`
-  );
+	console.log(`Fork of ${req.body.projectId} was created. New Project ID: ${_id}`);
 
-  return res.json({ projectId: _id, owner: req.user });
+	return res.json({ projectId: _id, owner: req.user });
 });
 
-app.post("/pull/:originalProjectId", decodeIDToken, async (req, res) => {
-  /**
-   * the projectID in the req.params is the original project ID, i.e. "where to pull"
-   * the projectID in the req.body is the forked project ID, i.e. "from where to pull"
-   */
-  const { originalProjectId } = req.params;
-  const { forkedProjectId } = req.body;
+app.post('/pull/:originalProjectId', decodeIDToken, async (req, res) => {
+	/**
+	 * the projectID in the req.params is the original project ID, i.e. "where to pull"
+	 * the projectID in the req.body is the forked project ID, i.e. "from where to pull"
+	 */
+	const { originalProjectId } = req.params;
+	const { forkedProjectId } = req.body;
 
-  const { template, data, options } = await Project.findById(forkedProjectId);
-  await Project.findByIdAndUpdate(originalProjectId, {
-    template,
-    data,
-    options,
-  });
+	const { template, data, options } = await Project.findById(forkedProjectId);
+	await Project.findByIdAndUpdate(originalProjectId, {
+		template,
+		data,
+		options,
+	});
 
-  return res.json({ template, data, options });
+	return res.json({ template, data, options });
 });
 
-app.get("/projects", decodeIDToken, async (req, res) => {
-  let projects = [];
-  if (req.user) {
-    console.log("Owner:", req.user._id);
-    projects = await Project.find({ owner: req.user._id });
-    // console.log(projects);
-  }
-  return res.json(projects);
+app.get('/projects', decodeIDToken, async (req, res) => {
+	let projects = [];
+	if (req.user) {
+		console.log('Owner:', req.user._id);
+		projects = await Project.find({ owner: req.user._id });
+		// console.log(projects);
+	}
+	return res.json(projects);
 });
 
 app.use('/project', projectRoutes);
